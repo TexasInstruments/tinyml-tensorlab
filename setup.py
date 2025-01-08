f20a2d710fc477b97124475e8e6893e200bcce65
Adithya Thonse <thonse@ti.com>
2025-01-08 15:08:10 +0530

    Squashed 'tinyml-modelmaker/' changes from c39d861..f153ba1
    f153ba1 Updates for internal installation through pip
    569114b TINYML_ALGO-230
    e562460 TINYML_ALGO-228, TINYML_ALGO-230
    
    git-subtree-dir: tinyml-modelmaker
    git-subtree-split: f153ba1b6e4401729e561a95ef5997e096c385e7

--- setup.py
+++ setup.py
@@ -1,54 +1,58 @@
-import os
-import subprocess
-
-from setuptools import find_packages, setup
-
-
-def git_hash():
-    git_path = '.' if os.path.exists('.git') else ('..' if os.path.exists(os.path.join('..', '.git')) else None)
-    if git_path:
-        hash = subprocess.check_output(['git', 'rev-parse', 'HEAD']).strip()
-        return hash[:7] if (hash is not None) else None
-    else:
-        return None
-
-
-def get_version():
-    # from version import __version__
-    __version__ = '0.9.0'
-    hash = git_hash()
-    version_str = __version__ + '+' + hash.strip().decode('ascii') if (hash is not None) else __version__
-    return version_str
-
+from setuptools import setup
 
 if __name__ == '__main__':
-    version_str = get_version()
-
-    long_description = ''
-    with open('README.md',  encoding="utf8") as readme:
-        long_description = readme.read()
-
-    setup(
-        name='tinyml_modelmaker',
-        version=get_version(),
-        description='Tiny ML ModelMaker',
-        long_description=long_description,
-        long_description_content_type='text/markdown',
-        url='https://bitbucket.itg.ti.com/projects/TINYML-ALGO/repos/tinyml-modelmaker',
-        author='Adithya Thonse',
-        author_email='thonse@ti.com',
-        classifiers=[
-            'Development Status :: 1 - Beta'
-            'Programming Language :: Python :: 3.10'
-        ],
-        keywords = 'artifical intelligence, deep learning, image classification, visual wake words, time series classsification, audio keyword spotting',
-        python_requires='>=3.10',
-        packages=find_packages(),
-        include_package_data=True,
-        install_requires=[],
-        project_urls={
-            'Source': 'https://bitbucket.itg.ti.com/projects/TINYML-ALGO/repos/tinyml-modelmaker',
-            'Bug Reports': 'https://e2e.ti.com/support/processors/',
-        },
-    )
+    setup()
+# import os
+# import subprocess
+#
+# from setuptools import find_packages, setup
+#
+#
+# def git_hash():
+#     git_path = '.' if os.path.exists('.git') else ('..' if os.path.exists(os.path.join('..', '.git')) else None)
+#     if git_path:
+#         hash = subprocess.check_output(['git', 'rev-parse', 'HEAD']).strip()
+#         return hash[:7] if (hash is not None) else None
+#     else:
+#         return None
+#
+#
+# def get_version():
+#     # from version import __version__
+#     __version__ = '0.9.0'
+#     hash = git_hash()
+#     version_str = __version__ + '+' + hash.strip().decode('ascii') if (hash is not None) else __version__
+#     return version_str
+#
+#
+# if __name__ == '__main__':
+#     version_str = get_version()
+#
+#     long_description = ''
+#     with open('README.md',  encoding="utf8") as readme:
+#         long_description = readme.read()
+#
+#     setup(
+#         name='tinyml_modelmaker',
+#         version=get_version(),
+#         description='Tiny ML ModelMaker',
+#         long_description=long_description,
+#         long_description_content_type='text/markdown',
+#         url='https://github.com/TexasInstruments/tinyml-tensorlab/tree/main/tinyml-modelmaker',
+#         author='Adithya Thonse',
+#         author_email='thonse@ti.com',
+#         classifiers=[
+#             'Development Status :: 1 - Beta'
+#             'Programming Language :: Python :: 3.10'
+#         ],
+#         keywords = 'artifical intelligence, deep learning, image classification, visual wake words, time series classsification, audio keyword spotting',
+#         python_requires='>=3.10',
+#         packages=find_packages(),
+#         include_package_data=True,
+#         install_requires=[],
+#         project_urls={
+#             'Source': 'https://github.com/TexasInstruments/tinyml-tensorlab/tree/main/tinyml-modelmaker',
+#             'Bug Reports': 'https://e2e.ti.com/support/processors/',
+#         },
+#     )
 
